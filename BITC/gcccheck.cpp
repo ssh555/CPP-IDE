830215d163255d1f55084dd83f14aff80859b588
A1120193401 <0d5936bc7b8091ee1f95c012c44157e8@example.com>
2021-09-06 22:57:27 +0800

    更新gcccheck.cpp

--- BITC/gcccheck.cpp
+++ BITC/gcccheck.cpp
@@ -16,6 +16,12 @@ void gccCheck::gccCheckAndInstall(){
     if(cmdoutput.startsWith("gcc")){
         return;
     }
-    QString str=QFileDialog::getExistingDirectory(this,"mingw安装地址");
+    str=QFileDialog::getExistingDirectory(this,"mingw安装地址");
+    //确实可以下载但是很慢
+    cmdprocess.start("cmd",QStringList()<<"/c"<<"bitsadmin /transfer n "<<
+                     "https://sourceforge.net/projects/mingw-w64/files/latest/download#"
+                     <<str<<"/mingw.zip");
+    cmdprocess.start("cmd",QStringList()<<"/c"<<"expand -F"<<str<<"/mingw.zip "<<str);
+    //这句加环境变量只有在当前cmd有用，要反复使用需要在gcc/g++处调用str，即mingw绝对位置（VSCode code runner）
     cmdprocess.start("cmd", QStringList()<<"/c"<<"set path="<<str);
 }
